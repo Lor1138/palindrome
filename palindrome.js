be09842558788c207ffb7cce158a33c0a23c2334
Laura OBrien <lauraobdev@gmail.com>
2024-05-27 09:49:28 -0400

    finished logic

--- palindrome.js
+++ palindrome.js
@@ -1,15 +1,13 @@
 
-const inputContainer = document.querySelector("#palindrone-main");
+const inputContainer = document.querySelector("#palindrome-main");
 const inputWord = document.querySelector("#text-input");
 const checkButton = document.querySelector("#check-btn");
-const definitionOutput = document.querySelector("#result-definition")
-
-
+const definitionOutput = document.querySelector("#result")
 
 
 const isPalindrome = (input) => {
   //clean up string
-  const origInput = input.replace(/[^A-Za-z0-9]/g, " ").toLowerCase();
+  const origInput = input.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
   //reverse the string then return joined
   const reversedInput = origInput
     .split("")
@@ -21,10 +19,10 @@ const isPalindrome = (input) => {
   }
   
   //check if palindrome
-  if (origInput === reversedInput) {
-    return `${input} is a palindrome`
+  if (origInput !== reversedInput) {
+    result.textContent = `${input} is not a palindrome`
   } else {
-    return `${input} is not a palindrome`
+    result.textContent = `${input} is a palindrome`
    }
   
 }
@@ -32,6 +30,7 @@ checkButton.addEventListener("click", () => {
   const result = isPalindrome(inputWord.value);
   console.log(result);
   inputWord.value = " "
+  
   
 })
 
